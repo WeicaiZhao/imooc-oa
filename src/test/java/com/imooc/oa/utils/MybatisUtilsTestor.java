86094dc2567900b97d5246d9ccea5e333abd826f
WeicaiZhao <wz1842@mun.ca>
2020-12-15 23:16:42 -0330

    indicate some test bugs

--- src/test/java/com/imooc/oa/utils/MybatisUtilsTestor.java
+++ src/test/java/com/imooc/oa/utils/MybatisUtilsTestor.java
@@ -3,10 +3,12 @@ package com.imooc.oa.utils;
 import org.junit.Test;
 
 public class MybatisUtilsTestor {
+    //test got some problem
     @Test
     public void testcase1(){
         String result = (String)MybatisUtils.executeQuery(sqlSession -> {
             String out = (String)sqlSession.selectOne("test.sample");
+//            String out = (String)sqlSession.selectOne("test.sample");
             return out;
         });
         System.out.println(result);
